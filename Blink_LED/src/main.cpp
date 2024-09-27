bdcac8567c7ccc672722cbc6297e7491afa3adb6
dflindt <df@dflindtatflindt.com>
2024-09-27 18:14:33 +0200

    Added blink function

--- Blink_LED/src/main.cpp
+++ Blink_LED/src/main.cpp
@@ -1,40 +1,46 @@
 #include <Wire.h>
 #include <VL53L0X.h>
-
 VL53L0X sensor;
+
 #define LED 17
 
-void setup()
-{
+    void setup(){
+    Serial.begin(9600);
+    Wire.begin();
+    sensor.init();
 
-  Serial.begin(9600);
-  Wire.begin();
-  sensor.init();
-  sensor.setTimeout(500);
-  sensor.startContinuous();
+    sensor.setTimeout(500);
+    sensor.startContinuous();
 
-  pinMode(LED, OUTPUT);
+    pinMode(LED, OUTPUT);
+
 }
 
-void loop()
-{
-  int distance = sensor.readRangeContinuousMillimeters();
-  //int distance =sensor.startContinuous(100);
-  
- //distance = distance;
-  
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.print("mm");
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  
-  if (distance < 500){
-    digitalWrite(LED, HIGH);
-  }
-  else{
-    digitalWrite(LED, LOW);
-  }
+static int onTime = 100;
+static int offTime = 10;
+void blinkLed(){
+    static int time = 0;
+    if (time >= onTime){
+        digitalWrite(LED, LOW);
+    }
+    
+    if (time >= offTime+onTime){
+        digitalWrite(LED, HIGH);
+        time = 0;
+    }
+    time++;
+}
 
-  Serial.println();
-  delay(100);
+void loop(){
+    int distance = sensor.readRangeContinuousMillimeters();
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.print("mm");
+    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    
+    blinkLed();
+    
+    Serial.println();
+    delay(1);
 }
