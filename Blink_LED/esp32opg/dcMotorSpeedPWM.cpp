3b88dab56ec9599be535926e10ba850d19551cc2
dflindt <df@dflindtatflindt.com>
2024-10-02 20:20:31 +0200

    tilføjede h-bro2, motorstyring kan dreje på venstre side

--- Blink_LED/esp32opg/dcMotorSpeedPWM.cpp
+++ Blink_LED/esp32opg/dcMotorSpeedPWM.cpp
@@ -3,56 +3,94 @@
 
 VL53L0X sensor;
 //Forreste venstre motor
-#define motorFL1 5 
-#define motorFL2 18
+#define motorFL1 4
+#define motorFL2 2
 
 //Forreste højre motor
-#define motorFR1 2 
-#define motorFR2 4
+#define motorFR1 5
+#define motorFR2 18
+
+//Bagerste venstre motor
+#define motorBL1 25
+#define motorBL2 26
+
+//Bagerste højre motor
+#define motorBR1 14
+#define motorBR2 27
 
 #define LED 17
 
-int pins[] = {LED, motorFL1, motorFL2, motorFR1, motorFR2}; //Pins der skal sættes som output
-int motorSpeeds[] = {0, 120, 160, 255};
+//Pins der skal sættes som output
+int pins[] = {LED, motorFL1, motorFL2, motorFR1, motorFR2, motorBL1, motorBL2, motorBR1, motorBR2}; 
+int motorSpeeds[] = {0, 150, 190, 255};
+int reactDistance = 50; //Distance hvis sensor læser noget under, drej.
 
-void setup()
-{
-
+void setup(){
   Serial.begin(115200);
   Wire.begin();
   sensor.init();
   sensor.setTimeout(500);
   sensor.startContinuous();
 
+  //Finder længden af pins array, sætter pins i array som digital eller analog output
   int pinArrayLen = sizeof(pins)/sizeof(pins[0]);
   for(int i; i < pinArrayLen; i++){
     pinMode(pins[i], OUTPUT);
     analogWrite(pins[i], 0);
   }
 
+}
+
+void SetLeftMotorsSpeed(int speed){ //hastighed 0 1 2 eller 3 fra motorSpeeds array
+  analogWrite(motorFL1, motorSpeeds[speed]);
+  analogWrite(motorBL1, motorSpeeds[speed]);
+  
+}
+
+void SetRightMotorsSpeed(int motorSpeeds[], int time){
+
 
 }
 
-void loop()
-{
+void stopMotors(){
+  analogWrite(motorFL1, motorSpeeds[0]);
+  analogWrite(motorBL1, motorSpeeds[0]);
+
+  analogWrite(motorFR1, motorSpeeds[0]);
+  analogWrite(motorBR1, motorSpeeds[0]);
+}
+
+void loop(){
   int distance = sensor.readRangeContinuousMillimeters();
   Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.print("mm");
-
-  //if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.print("mm ");
   
-  if (distance <= 400){
-    analogWrite(motorFL1, motorSpeeds[3]);
-    analogWrite(motorFR1, motorSpeeds[3]);
+  
+  if (distance <= reactDistance){
+    //kør langsommere på højre / drej venstre
+    SetLeftMotorsSpeed(1);
+    //analogWrite(motorFL1, motorSpeeds[3]);
+    //analogWrite(motorBL1, motorSpeeds[3]);
+
+    //analogWrite(motorFR1, motorSpeeds[1]);
+    //analogWrite(motorBR1, motorSpeeds[1]);
+
     analogWrite(LED, 255);
   }
-  else if (distance > 400){
-    analogWrite(motorFL1, motorSpeeds[0]);
-    analogWrite(motorFR1, motorSpeeds[0]);
+  else if (distance > reactDistance){
+    //kør alle motore
+    // analogWrite(motorFL1, motorSpeeds[3]);
+    // analogWrite(motorBL1, motorSpeeds[3]);
+
+    // analogWrite(motorFR1, motorSpeeds[3]);
+    // analogWrite(motorBR1, motorSpeeds[3]);
+    
+    stopMotors();
     analogWrite(LED, 0);
   }
 
   Serial.println();
   delay(1);
+
 }
--- Blink_LED/src/main.cpp
+++ Blink_LED/src/main.cpp
@@ -1,46 +1,96 @@
 #include <Wire.h>
 #include <VL53L0X.h>
+
 VL53L0X sensor;
+//Forreste venstre motor
+#define motorFL1 4
+#define motorFL2 2
+
+//Forreste højre motor
+#define motorFR1 5
+#define motorFR2 18
+
+//Bagerste venstre motor
+#define motorBL1 25
+#define motorBL2 26
+
+//Bagerste højre motor
+#define motorBR1 14
+#define motorBR2 27
 
 #define LED 17
 
-    void setup(){
-    Serial.begin(9600);
-    Wire.begin();
-    sensor.init();
+//Pins der skal sættes som output
+int pins[] = {LED, motorFL1, motorFL2, motorFR1, motorFR2, motorBL1, motorBL2, motorBR1, motorBR2}; 
+int motorSpeeds[] = {0, 150, 190, 255};
+int reactDistance = 50; //Distance hvis sensor læser noget under, drej.
 
-    sensor.setTimeout(500);
-    sensor.startContinuous();
+void setup(){
+  Serial.begin(115200);
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+  sensor.startContinuous();
 
-    pinMode(LED, OUTPUT);
+  //Finder længden af pins array, sætter pins i array som digital eller analog output
+  int pinArrayLen = sizeof(pins)/sizeof(pins[0]);
+  for(int i; i < pinArrayLen; i++){
+    pinMode(pins[i], OUTPUT);
+    analogWrite(pins[i], 0);
+  }
 
 }
 
-static int onTime = 100;
-static int offTime = 10;
-void blinkLed(){
-    static int time = 0;
-    if (time >= onTime){
-        digitalWrite(LED, LOW);
-    }
-    
-    if (time >= offTime+onTime){
-        digitalWrite(LED, HIGH);
-        time = 0;
-    }
-    time++;
+void SetLeftMotorsSpeed(int speed){ //hastighed 0 1 2 eller 3 fra motorSpeeds array
+  analogWrite(motorFL1, motorSpeeds[speed]);
+  analogWrite(motorBL1, motorSpeeds[speed]);
+  
+}
+
+void SetRightMotorsSpeed(int motorSpeeds[], int time){
+
+
+}
+
+void stopMotors(){
+  analogWrite(motorFL1, motorSpeeds[0]);
+  analogWrite(motorBL1, motorSpeeds[0]);
+
+  analogWrite(motorFR1, motorSpeeds[0]);
+  analogWrite(motorBR1, motorSpeeds[0]);
 }
 
 void loop(){
-    int distance = sensor.readRangeContinuousMillimeters();
+  int distance = sensor.readRangeContinuousMillimeters();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print("mm ");
+  
+  
+  if (distance <= reactDistance){
+    //kør langsommere på højre / drej venstre
+    SetLeftMotorsSpeed(1);
+    //analogWrite(motorFL1, motorSpeeds[3]);
+    //analogWrite(motorBL1, motorSpeeds[3]);
 
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.print("mm");
-    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    //analogWrite(motorFR1, motorSpeeds[1]);
+    //analogWrite(motorBR1, motorSpeeds[1]);
+
+    analogWrite(LED, 255);
+  }
+  else if (distance > reactDistance){
+    //kør alle motore
+    // analogWrite(motorFL1, motorSpeeds[3]);
+    // analogWrite(motorBL1, motorSpeeds[3]);
+
+    // analogWrite(motorFR1, motorSpeeds[3]);
+    // analogWrite(motorBR1, motorSpeeds[3]);
     
-    blinkLed();
-    
-    Serial.println();
-    delay(1);
+    stopMotors();
+    analogWrite(LED, 0);
+  }
+
+  Serial.println();
+  delay(1);
+
 }
