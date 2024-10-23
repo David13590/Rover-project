8402bd9f25d647d39bc16c9cde3506f9319b9292
dflindt <df@dflindtatflindt.com>
2024-10-23 22:24:31 +0200

    cleanup

--- Blink_LED/src/gesture.cpp
+++ Blink_LED/src/gesture.cpp
@@ -3,31 +3,28 @@
 #include "gesture.hpp"
 
 void gestureSetup(){
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(buttonPin, INPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
 }
- 
-
 
 bool gesture(){
     static int state = 0;
     static int startTime = 0;
     static int endTime = 0;
     static int lastTime = 0;
-    bool i = digitalRead(buttonPin);
+    bool senseGesture = digitalRead(buttonPin);
     bool gesture = false;
-    digitalWrite(LED_BUILTIN, i);
+    digitalWrite(LED_BUILTIN, senseGesture);
 
     if(state == 0){
-        if(i == HIGH){
+        if(senseGesture == HIGH){
             state = 1;
             startTime = millis();
         }
     }
-
     if(state == 1){
-        if(i == HIGH){
+        if(senseGesture == HIGH){
             lastTime = millis();
         }
         if(lastTime - startTime > 200){
@@ -35,19 +32,16 @@ bool gesture(){
             state = 2;
         }
     } 
-
     if(state == 2){
-        if(i == HIGH){
+        if(senseGesture == HIGH){
             lastTime = millis();
         }
         else{
             endTime = millis();
         }
-
         if(endTime - lastTime > 500){
             state = 0;
         }
     } 
-
     return gesture;   
 }
